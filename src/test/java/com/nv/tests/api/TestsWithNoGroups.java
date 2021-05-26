ec39f56e2043011c191059881a495c7d6b7a83fb
niharikav <niharika.varshney@gmail.com>
2021-05-26 22:36:01 +0530

    Adding

--- src/test/java/com/nv/tests/api/TestsWithNoGroups.java
+++ src/test/java/com/nv/tests/api/TestsWithNoGroups.java
@@ -9,22 +9,19 @@ public class TestsWithNoGroups {
 
 
 
-    @Test( groups = {"NVPROJ-TC-3"}, description = "Description for failing test",
+    @Test( groups = {"NVTES-TC-3"}, description = "Description for failing test",
             attributes = {@CustomAttribute(name="aio_key", values = "tcname")})
     public void failingTest() {
-        try{
-//            Thread.sleep(60000);
-        } catch (Exception e) { }
         Reporter.log("See what happens here");
         Assert.assertEquals(1,1, "done");
     }
 
-    @Test( groups = {"NVPROJ-TC-4"}, description = "Description for failing test")
+    @Test( groups = {"NVTES-TC-4"}, description = "Description for failing test")
     public void failingTest2() {
         Assert.assertEquals(0,1, "done");
     }
 
-    @Test( groups = {"NVPROJ-TC-6"}, description = "Description for failing test",
+    @Test( groups = {"NVTES-TC-6"}, description = "Description for failing test",
             attributes = {@CustomAttribute(name="aio_key", values = "tcname")})
     public void failingTest3() {
 
